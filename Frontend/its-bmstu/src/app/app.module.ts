67bd1511b0b71303bd610a621bcf4e740d493ef6
heyuaresocute <nastya.gummy@gmail.com>
2023-03-01 18:01:01 +0300

    Site-1. Add bootstrap 5. Add npm and css link.

--- Frontend/its-bmstu/src/app/app.module.ts
+++ Frontend/its-bmstu/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
---
+++ Frontend/its-bmstu/src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+}

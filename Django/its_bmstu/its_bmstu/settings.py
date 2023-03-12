496fe804c7dbc7dada00e2a60ff16eb38dc10227
IvanNaum <nuk04@mail.ru>
2023-03-13 00:12:21 +0300

    Init Django Rest Framework

--- Django/its_bmstu/its_bmstu/settings.py
+++ Django/its_bmstu/its_bmstu/settings.py
@@ -15,6 +15,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -22,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'actions',
 ]
 

ff2a51ac4742bdd1790c05e56a3f50c370e68552
IvanNaum <nuk04@mail.ru>
2023-03-30 15:24:33 +0300

    debug pagination

--- Django/its_bmstu/actions/views.py
+++ Django/its_bmstu/actions/views.py
@@ -15,14 +15,14 @@ class ActionDetail(generics.RetrieveAPIView):
 
 
 class ActionList(generics.ListAPIView):
-    queryset = Action.objects.all()
+    queryset = Action.objects.all().order_by('-pk')
     serializer_class = ActionListSerializer
     pagination_class = ActionPagination
 
 
 class PhotoList(views.APIView, PhotoPagination):
     def get(self, request, slug):
-        photos = Photo.objects.filter(action__slug=slug)
+        photos = Photo.objects.filter(action__slug=slug).order_by('pk')
         photos = self.paginate_queryset(photos, request, view=self)
         serializer = PhotoSerializer(photos, many=True,
                                      context={'request': request})

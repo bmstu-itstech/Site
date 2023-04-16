959a31226d2808b47437402496f04fa01c77a0eb
heyuaresocute <nastya.gummy@gmail.com>
2023-04-16 17:21:03 +0300

    navigate to about card on navigation bar clicked

--- Frontend/its-bmstu/src/app/pages/navigation-bar/navigation-bar.component.ts
+++ Frontend/its-bmstu/src/app/pages/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  clickAboutMenuItem(): void {
+    const currentPage = this.router.url;
+    console.log("Page is" + currentPage);
+
+    if (currentPage !== '')
+      this.router.navigate([''], {state: {scrollToAboutCard: true}})
+        .then(() => {
+          if (document !== undefined && document !== null) {
+
+            let element = document.getElementById("about-us-id");
+
+            if (element) {
+              const rect = element.getBoundingClientRect();
+              const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+              const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+              const top = rect.top + scrollTop - 50;
+              const left = rect.left + scrollLeft;
+              window.scrollTo({top, left, behavior: 'smooth'});
+            }
+
+            // if (element !== null) {
+            //   //   element.scrollIntoView({behavior: 'smooth'});
+            //   //const id = 'profilePhoto';
+            //   const yOffset = -30;
+            //
+            //
+            //   //const y = element.get + yOffset;
+            //   //console.log("y is " + y);
+            //
+            //   window.scrollTo({top: y, behavior: 'smooth'});
+            //
+            // }
+          }
+        });
+  }
 
 }

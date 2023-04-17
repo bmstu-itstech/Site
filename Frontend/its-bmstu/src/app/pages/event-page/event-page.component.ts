b839e180b2483825efe66f06eca218a2b460b863
heyuaresocute <nastya.gummy@gmail.com>
2023-04-17 21:34:40 +0300

    gallery in progress

--- Frontend/its-bmstu/src/app/pages/event-page/event-page.component.ts
+++ Frontend/its-bmstu/src/app/pages/event-page/event-page.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {UrlsProviderService} from "../../../services/urls-provider.service";
 import {Photo} from "./photo";
 import {Router} from "@angular/router";
+import {toArray} from "rxjs";
+import {GalleryItem, ImageItem} from "ng-gallery";
 
 @Component({
   selector: 'app-event-page',
@@ -10,6 +12,11 @@ import {Router} from "@angular/router";
 })
 export class EventPageComponent {
   photos: Photo[] = [];
+  images = [
+
+    // ... more items
+  ];
+  //galleryPhotos: ImageItem[] = [];
   columnSizes: number[] | undefined;
   firstPhotoStyleBackground2: string = '';
   private _urlsProviderService: UrlsProviderService;
@@ -41,7 +48,11 @@ export class EventPageComponent {
         for (let i = 0; i < photos.count; i++) {
           let imageSrc = photos.results[i].photo;
           this.photos.push(new Photo(imageSrc, null, null, this.columnSizes[i]));
+          //this.galleryPhotos.push(new ImageItem({src: 'IMAGE_SRC_URL', thumb: 'IMAGE_THUMBNAIL_URL'}));
+          // @ts-ignore
+          this.images.push(new ImageItem({src: imageSrc, thumb: imageSrc}));
         }
+
 
         this.firstPhotoStyleBackground2 = `linear-gradient(to bottom, rgba(20, 16, 75, 0) 0%, #14104B 100%)`;
       });
@@ -55,5 +66,8 @@ export class EventPageComponent {
   goBack() {
     this.router.navigate(['/']);
   }
+
+  protected readonly toArray = toArray;
+  protected readonly String = String;
 }
 
--- Frontend/its-bmstu/src/app/pages/event-page/photo.ts
+++ Frontend/its-bmstu/src/app/pages/event-page/photo.ts
@@ -1,17 +1,28 @@
 import {EventDto} from "../main-page/events-collection/events-collection.component";
+import {GalleryItem, ImageItem} from "ng-gallery";
+import {GalleryItemData, ImageItemData} from "ng-gallery/lib/components/templates/items.model";
 
-export class Photo {
+export class Photo implements ImageItem {
   imageSrc: string;
   columnSize: number;
   columnClass: string;
   event: EventDto | null = null;
   url: string | null = null;
+  src: string | null = null;
+  thumb: string;
 
   constructor(imageSrc: string, url: string | null, event: EventDto | null, columnSize: number = 6) {
     this.url = url;
+    this.src = imageSrc;
+    this.thumb = imageSrc;
     this.event = event;
     this.imageSrc = imageSrc;
     this.columnSize = columnSize;
     this.columnClass = "col-" + columnSize;
+    this.type = "image";
+    this.data = {} as ImageItemData;
   }
+
+  readonly data: ImageItemData;
+  readonly type: string;
 }

9e58802b349ff1bed49de501fc6de295f7a215a3
heyuaresocute <nastya.gummy@gmail.com>
2023-04-23 15:22:42 +0300

    fix background

--- Frontend/its-bmstu/src/app/pages/main-page/our-partners/our-partners.component.ts
+++ Frontend/its-bmstu/src/app/pages/main-page/our-partners/our-partners.component.ts
@@ -6,7 +6,7 @@ import {PartnersDto} from "../../../../services/partners-dto";
 @Component({
   selector: 'app-our-partners',
   templateUrl: './our-partners.component.html',
-  styleUrls: ['./our-partners.component.css']
+  styleUrls: ['./our-partners.component.scss']
 })
 export class OurPartnersComponent implements OnInit {
 
